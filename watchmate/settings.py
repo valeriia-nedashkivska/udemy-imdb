9448dd7540a41bd78771475dc880794e369a82d8
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-06-15 10:55:00 +0300

    Add movie list API

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
