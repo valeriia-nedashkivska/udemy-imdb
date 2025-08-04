922c2d4821b8a608a290500fa15279f352324ffc
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-08-04 19:53:09 +0300

    Add user registration

---
+++ user_app/api/views.py
@@ -0,0 +1,28 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from user_app import models
+from user_app.api.serializers import RegistrationSerializer
+
+
+@api_view(['POST', ])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+
+        data = {}
+
+        if serializer.is_valid():
+            account = serializer.save()
+
+            data['response'] = "Registration successful"
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+
+        else:
+            data = serializer.errors
+
+        return Response(data)
