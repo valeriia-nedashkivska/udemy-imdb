fb4c8c0e4dcaa0f3a7fe611258dfa78bc0c38faf
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-08-12 16:06:04 +0300

    Add pagination - LimitOffsetPagination

--- watchlist_app/api/pagination.py
+++ watchlist_app/api/pagination.py
@@ -1,4 +1,4 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 
 class WatchlistPagination(PageNumberPagination):
@@ -7,3 +7,10 @@ class WatchlistPagination(PageNumberPagination):
     page_size_query_param = 'size'
     max_page_size = 10
     last_page_strings = 'end'
+
+
+class WatchlistLOPagination(LimitOffsetPagination):
+    default_limit = 5
+    max_limit = 10
+    limit_query_param = 'limit'
+    offset_query_param = 'start'
