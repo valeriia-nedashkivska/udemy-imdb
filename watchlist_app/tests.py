bcec327003d324977fb9f50ba325578c7db0a141
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-08-17 21:06:17 +0300

    Add API Testing - Watchlist

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -45,3 +45,35 @@ class StreamPlatformTestCase(APITestCase):
     def test_streamplatform_ind(self):
         response = self.client.get(reverse('streamplatform-detail', args=(self.stream.id,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class WatchlistTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="Password@123")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name="Netflix", about="#1 Platform",
+                                                           website="https://www.netflix.com")
+        self.watchlist = models.Watchlist.objects.create(platform=self.stream, title="Example Movie",
+                                                         storyline="Example Movie", active=True)
+
+    def test_watchlist_create(self):
+        data = {
+            "platform": self.stream,
+            "title": "Example Movie",
+            "storyline": "Example Story",
+            "active": True
+        }
+        response = self.client.post(reverse('movie-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('movie-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_watchlist_ind(self):
+        response = self.client.get(reverse('movie-detail', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.Watchlist.objects.count(), 1)
+        self.assertEqual(models.Watchlist.objects.get().title, 'Example Movie')
