ec836db328dd5d8d6468e7058e461527241c4715
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-08-17 20:10:58 +0300

    Add API Testing - StreamPlatform

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from watchlist_app.api import serializers
+from watchlist_app import models
 
-# Create your tests here.
+
+class StreamPlatformTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="Password@123", is_staff=True)
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name="Netflix", about="#1 Streaming Platform",
+                                                           website="https://netflix.com")
+
+    def test_streamplatform_create(self):
+        data = {
+            "name": "Netflix",
+            "about": "#1 Streaming Platform",
+            "website": "https://netflix.com",
+        }
+        response = self.client.post(reverse('streamplatform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_streamplatform_update(self):
+        data = {
+            "name": "Netflix",
+            "about": "#1 Streaming Platform - Update",
+            "website": "https://netflix.com",
+        }
+        response = self.client.put(reverse('streamplatform-detail', args=(self.stream.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_streamplatform_delete(self):
+        response = self.client.delete(reverse('streamplatform-detail', args=(self.stream.id,)))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_streamplatform_list(self):
+        response = self.client.get(reverse('streamplatform-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_streamplatform_ind(self):
+        response = self.client.get(reverse('streamplatform-detail', args=(self.stream.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
